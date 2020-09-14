2f22142dc1574d72a1dfbefd7e7981175fd038c4
LIAO-JIAN-PENG <53342598+LIAO-JIAN-PENG@users.noreply.github.com>
2020-09-14 23:31:56 +0800

    改回去爬選課系統(強調還未整理code)
    這次終於解決POST問題，然後也還未整理，所以僅供參考，但是時間應該是做好了，明天有空再弄

--- Course_Table_scraper/scarper.py
+++ Course_Table_scraper/scarper.py
@@ -1,47 +1,31 @@
 import requests
 import bs4
-
-# variable part
-course_collection = {}
-#======================#
+import re
 
 req = requests.Session()  # Seesion 儲存傳給伺服器的(對話)cookies
 
-#=====================#
-student_id = 'a1085508'
-#=====================#
+# ==================== #
+# 帳號密碼
+account = 'a1085508'
+password = 's125367799'
+# ==================== #
+headers={
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+}
 
-# post elearning
-req.post('http://elearning.nuk.edu.tw/p_login_stu_at.php?bStu_id='+student_id)
-r = req.get('http://elearning.nuk.edu.tw/m_student/m_stu_index.php')
-r.encoding = 'utf-8'
+req.post('https://course.nuk.edu.tw/Sel/SelectMain1.asp',{'Account':account,'Password':password})
+r=req.get('https://course.nuk.edu.tw/Sel/roomlist1.asp', headers=headers)
+r.encoding = 'big5'
 
-# make a soup
-soup = bs4.BeautifulSoup(r.text,'html.parser')
+soup =  bs4.BeautifulSoup(r.text, 'lxml')
+tr_list = soup.find_all('tr')
 
-# course_list
-course_list = soup.find('tbody').find_all('tr')
-
-# course collection
-# type
-# {'course_name':
-#               {
-#                 'time':'(3)_1_2_3',
-#                 'place':''
-#               }
-# }
-
-for cl in course_list:
-    course_data = cl.find_all('td')
-    
-    # fill the course_detail
-    course_detail = {}
-    course_detail['time']= course_data[4].text
-    course_detail['place']= course_data[5].text
-
-    # courses_collection with details
-    if course_data[1].text[:5] == '109-1':
-        course_collection[course_data[1].text[5:]] = course_detail
-
-# testing
-print(course_collection)
+weekday = 0
+time_period = 0
+for tr in tr_list:
+    td_list = tr.find_all('td')
+    for td in td_list[2:]:
+        if td.text != '　':
+            print('星期', weekday % 7 + 1, td.text, '第'+str(time_period%12+1)+'節')
+        weekday += 1
+    time_period += 1
